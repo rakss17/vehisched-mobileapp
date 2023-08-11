fb58b3080148f94e605f7a685c0e86027c7e73fd
rakss17 <bohariambulo17@gmail.com>
2023-08-11 14:22:13 +0800

    added mock data to the components folder temporarily

--- src/screens/Requester/requester.tsx
+++ src/screens/Requester/requester.tsx
@@ -10,61 +10,12 @@ import {
 import Header from "../../components/header/header";
 import Button from "../../components/buttons/button";
 import { Vehicle } from "../../interfaces/interfaces";
+import { vehiclesMockData } from "../../components/mockdata/mockdata";
 
-const fetchedVehicles: Vehicle[] = [
-  {
-    id: 1,
-    vehicle_name: "KDA 1368 Toyota Hilux",
-    capacity: 5,
-    vehicle_type: "Pickup Truck",
-    vehicle_image: require("../../../assets/toyota-hilux.jpg"),
-    status: "On Trip",
-  },
-  {
-    id: 2,
-    vehicle_name: "KCU 2522 Montero Sport",
-    capacity: 7,
-    vehicle_type: "SUV",
-    vehicle_image: require("../../../assets/mitsubishi-montero.jpg"),
-    status: "Available",
-  },
-  {
-    id: 3,
-    vehicle_name: "KAB 2855 Fortuner",
-    capacity: 7,
-    vehicle_type: "SUV",
-    vehicle_image: require("../../../assets/fortuner.jpg"),
-    status: "On Trip",
-  },
-  {
-    id: 4,
-    vehicle_name: "KYZ 2069 Toyota Hiace",
-    capacity: 15,
-    vehicle_type: "Van",
-    vehicle_image: require("../../../assets/toyota-hiace.png"),
-    status: "Available",
-  },
-  {
-    id: 5,
-    vehicle_name: "KDA 1368 Toyota Hilux",
-    capacity: 5,
-    vehicle_type: "Pickup Truck",
-    vehicle_image: require("../../../assets/toyota-hilux.jpg"),
-    status: "Unavailable",
-  },
-  {
-    id: 6,
-    vehicle_name: "KYZ 2069 Toyota Hiace",
-    capacity: 15,
-    vehicle_type: "Van",
-    vehicle_image: require("../../../assets/toyota-hiace.png"),
-    status: "Unavailable",
-  },
-];
 export default function Requester() {
   const [vehicles, setVehicles] = useState<Vehicle[]>([]);
   const fetchedVehicleList = () => {
-    setVehicles(fetchedVehicles);
+    setVehicles(vehiclesMockData);
   };
 
   useEffect(() => {
