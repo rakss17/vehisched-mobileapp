f35a2a9bba75d31d86c2f8aed0ff001b53a9193b
rakss17 <bohariambulo17@gmail.com>
2023-08-23 22:59:56 +0800

    schedules screen updates

--- src/screens/Driver/schedule.tsx
+++ src/screens/Driver/schedule.tsx
@@ -67,280 +67,286 @@ export default function Schedules() {
         style={{ width: Viewport.width * 1, height: Viewport.height * 0.04 }}
       />
       <Header />
-      <View
-        style={[
-          {
-            width: Viewport.width * 0.7,
-          },
-          Styles.flexRow,
-        ]}
-      >
-        <Text
+      <View style={Styles.container}>
+        <View
           style={[
             {
-              fontSize: FontSizes.normal,
-              color: Colors.primaryColor1,
-              fontWeight: "bold",
-              marginTop: 30,
-              marginLeft: 30,
+              width: Viewport.width * 0.7,
             },
+            Styles.flexRow,
           ]}
         >
-          Schedules
-        </Text>
-      </View>
-      <View
-        style={[
-          {
+          <Text
+            style={[
+              {
+                fontSize: FontSizes.normal,
+                color: Colors.primaryColor1,
+                fontWeight: "bold",
+                marginTop: Viewport.height * 0.2,
+              },
+            ]}
+          >
+            Schedules
+          </Text>
+        </View>
+        <View
+          style={[
+            {
+              width: Viewport.width * 1,
+              justifyContent: "space-around",
+              marginTop: 10,
+            },
+            Styles.flexRow,
+          ]}
+        >
+          <Button
+            text="Today"
+            transparentBG2
+            transparentText2
+            isHighlighted={selectedStatus === "Today"}
+            onPress={() => handleButtonPress("Today")}
+          />
+          <Button
+            text="In progress"
+            transparentBG2
+            transparentText2
+            isHighlighted={selectedStatus === "In Progress"}
+            onPress={() => handleButtonPress("In Progress")}
+          />
+          <Button
+            text="Upcoming"
+            transparentBG2
+            transparentText2
+            isHighlighted={selectedStatus === "Upcoming"}
+            onPress={() => handleButtonPress("Upcoming")}
+          />
+        </View>
+        <BackgroundColor
+          style={{
             width: Viewport.width * 1,
-            justifyContent: "space-around",
-            marginTop: 10,
-          },
-          Styles.flexRow,
-        ]}
-      >
-        <Button
-          text="Today"
-          transparentBG2
-          transparentText2
-          isHighlighted={selectedStatus === "Today"}
-          onPress={() => handleButtonPress("Today")}
+            height: Viewport.height * 0.01,
+            marginTop: Viewport.width * 0.02,
+          }}
         />
-        <Button
-          text="In progress"
-          transparentBG2
-          transparentText2
-          isHighlighted={selectedStatus === "In Progress"}
-          onPress={() => handleButtonPress("In Progress")}
-        />
-        <Button
-          text="Upcoming"
-          transparentBG2
-          transparentText2
-          isHighlighted={selectedStatus === "Upcoming"}
-          onPress={() => handleButtonPress("Upcoming")}
-        />
-      </View>
-      <BackgroundColor
-        style={{
-          width: Viewport.width * 1,
-          height: Viewport.height * 0.01,
-          marginTop: Viewport.width * 0.02,
-        }}
-      />
-      <View
-        style={[
-          {
-            paddingBottom: Viewport.height * 0.32,
-          },
-          Styles.flexColumn,
-        ]}
-      >
-        <ScrollView>
-          {scheduleData.length === 0 ? (
-            <Text
-              style={{
-                fontSize: FontSizes.small,
-                textAlign: "center",
-                marginTop: 15,
-              }}
-            >
-              No schedules available
-            </Text>
-          ) : (
-            scheduleData.map((schedule, index) => (
-              <View
-                key={index}
+        <View
+          style={[
+            {
+              paddingBottom: Viewport.height * 0.35,
+            },
+            Styles.flexColumn,
+          ]}
+        >
+          <ScrollView>
+            {scheduleData.length === 0 ? (
+              <Text
                 style={{
-                  backgroundColor: Colors.primaryColor2,
-                  marginTop: 20,
-                  paddingLeft: 20,
-                  width: Viewport.width * 0.95,
-                  height: Viewport.height * 0.4,
-                  elevation: 4,
+                  fontSize: FontSizes.small,
+                  textAlign: "center",
+                  marginTop: 15,
                 }}
               >
+                No schedules available
+              </Text>
+            ) : (
+              scheduleData.map((schedule, index) => (
                 <View
-                  style={[
-                    {
-                      width: Viewport.width * 0.7,
-                    },
-                    Styles.flexRow,
-                  ]}
-                >
-                  <Text
-                    style={[
-                      {
-                        fontSize: FontSizes.normal,
-                        color: Colors.secondaryColor2,
-                        fontWeight: "bold",
-                        marginTop: Viewport.height * 0.03,
-                        marginLeft: Viewport.width * 0.05,
-                      },
-                    ]}
-                  >
-                    Trip {schedule.trip_number}
-                  </Text>
-                </View>
-                <View
-                  style={[
-                    {
-                      width: Viewport.width * 0.7,
-                    },
-                    Styles.flexRow,
-                  ]}
-                >
-                  <Text
-                    style={[
-                      {
-                        fontSize: FontSizes.small,
-                        color: Colors.secondaryColor2,
-                        fontWeight: "bold",
-                        marginLeft: Viewport.width * 0.05,
-                        marginTop: Viewport.height * 0.03,
-                      },
-                    ]}
-                  >
-                    Date: {schedule.date}
-                  </Text>
-                  <Text
-                    style={[
-                      {
-                        fontSize: FontSizes.small,
-                        color: Colors.secondaryColor2,
-                        fontWeight: "bold",
-                        marginLeft: Viewport.width * 0.1,
-                        marginTop: Viewport.height * 0.03,
-                      },
-                    ]}
-                  >
-                    Time: {schedule.time}
-                  </Text>
-                </View>
-                <Text
-                  style={[
-                    {
-                      fontSize: FontSizes.small,
-                      color: Colors.secondaryColor2,
-                      marginLeft: Viewport.width * 0.05,
-                      marginTop: Viewport.height * 0.03,
-                    },
-                  ]}
-                >
-                  <Text style={{ fontWeight: "bold" }}>Destination:</Text>{" "}
-                  {schedule.destination}
-                </Text>
-                <View
+                  key={index}
                   style={{
-                    marginTop: Viewport.height * 0.03,
-                    height: Viewport.height * 0.09,
+                    backgroundColor: Colors.primaryColor2,
+                    marginTop: 20,
+                    paddingLeft: 20,
+                    width: Viewport.width * 0.95,
+                    height: Viewport.height * 0.4,
+                    elevation: 4,
                   }}
                 >
-                  <Text
+                  <View
                     style={[
                       {
-                        fontSize: FontSizes.small,
-                        color: Colors.secondaryColor2,
-                        marginLeft: Viewport.width * 0.05,
+                        width: Viewport.width * 0.7,
                       },
+                      Styles.flexRow,
                     ]}
                   >
-                    <Text style={{ fontWeight: "bold" }}>Passengers: </Text>
-                    {schedule.requester_name},{" "}
-                    {schedule.passenger_name &&
-                    schedule.passenger_name.length > 0 ? (
-                      schedule.passenger_name.length > 1 ? (
-                        <Text>
-                          {schedule.passenger_name.slice(0, 4).join(", ")}
-                          <Text
-                            onPress={() =>
-                              handleShowPassengersModal(schedule.passenger_name)
-                            }
-                            style={{ fontWeight: "normal" }}
-                          >
-                            {" "}
-                            and view more...
-                          </Text>
-                        </Text>
-                      ) : (
-                        schedule.passenger_name[0]
-                      )
-                    ) : (
-                      "No passenger(s)"
-                    )}
-                  </Text>
-                </View>
-
-                <View
-                  style={{
-                    alignItems: "center",
-                  }}
-                >
-                  <Button
-                    text={
-                      selectedStatus === "Upcoming"
-                        ? "Upcoming"
-                        : selectedStatus === "In Progress"
-                        ? "Completed?"
-                        : "QR code"
-                    }
-                    defaultBG
-                    onPress={() => handleShowQRCode(index)}
-                    disabled={selectedStatus === "Upcoming"}
-                    style={
-                      selectedStatus === "In Progress"
-                        ? { backgroundColor: "green" }
-                        : null
-                    }
-                  />
-                  {showQRCodeIndex === index && (
-                    <Modal
-                      animationType="fade"
-                      transparent={true}
-                      onRequestClose={handleHideQRCode}
+                    <Text
+                      style={[
+                        {
+                          fontSize: FontSizes.normal,
+                          color: Colors.secondaryColor2,
+                          fontWeight: "bold",
+                          marginTop: Viewport.height * 0.03,
+                          marginLeft: Viewport.width * 0.05,
+                        },
+                      ]}
                     >
-                      <View
-                        style={[
-                          {
-                            flex: 1,
-                            backgroundColor: "rgba(0, 0, 0, 0.5)",
-                          },
-                          Styles.flexColumn,
-                        ]}
+                      Trip {schedule.trip_number}
+                    </Text>
+                  </View>
+                  <View
+                    style={[
+                      {
+                        width: Viewport.width * 0.7,
+                      },
+                      Styles.flexRow,
+                    ]}
+                  >
+                    <Text
+                      style={[
+                        {
+                          fontSize: FontSizes.small,
+                          color: Colors.secondaryColor2,
+                          fontWeight: "bold",
+                          marginLeft: Viewport.width * 0.05,
+                          marginTop: Viewport.height * 0.03,
+                        },
+                      ]}
+                    >
+                      Date: {schedule.date}
+                    </Text>
+                    <Text
+                      style={[
+                        {
+                          fontSize: FontSizes.small,
+                          color: Colors.secondaryColor2,
+                          fontWeight: "bold",
+                          marginLeft: Viewport.width * 0.1,
+                          marginTop: Viewport.height * 0.03,
+                        },
+                      ]}
+                    >
+                      Time: {schedule.time}
+                    </Text>
+                  </View>
+                  <Text
+                    style={[
+                      {
+                        fontSize: FontSizes.small,
+                        color: Colors.secondaryColor2,
+                        marginLeft: Viewport.width * 0.05,
+                        marginTop: Viewport.height * 0.03,
+                      },
+                    ]}
+                  >
+                    <Text style={{ fontWeight: "bold" }}>Destination:</Text>{" "}
+                    {schedule.destination}
+                  </Text>
+                  <View
+                    style={{
+                      marginTop: Viewport.height * 0.03,
+                      height: Viewport.height * 0.09,
+                    }}
+                  >
+                    <Text
+                      style={[
+                        {
+                          fontSize: FontSizes.small,
+                          color: Colors.secondaryColor2,
+                          marginLeft: Viewport.width * 0.05,
+                        },
+                      ]}
+                    >
+                      <Text style={{ fontWeight: "bold" }}>Passengers: </Text>
+                      {schedule.requester_name},{" "}
+                      {schedule.passenger_name &&
+                      schedule.passenger_name.length > 0 ? (
+                        schedule.passenger_name.length > 1 ? (
+                          <Text>
+                            {schedule.passenger_name.slice(0, 4).join(", ")}
+                            <Text
+                              onPress={() =>
+                                handleShowPassengersModal(
+                                  schedule.passenger_name
+                                )
+                              }
+                              style={{ fontWeight: "normal" }}
+                            >
+                              {" "}
+                              and view more...
+                            </Text>
+                          </Text>
+                        ) : (
+                          schedule.passenger_name[0]
+                        )
+                      ) : (
+                        "No passenger(s)"
+                      )}
+                    </Text>
+                  </View>
+
+                  <View
+                    style={{
+                      alignItems: "center",
+                    }}
+                  >
+                    <Button
+                      text={
+                        selectedStatus === "Upcoming"
+                          ? "Upcoming"
+                          : selectedStatus === "In Progress"
+                          ? "Completed?"
+                          : "QR code"
+                      }
+                      defaultBG
+                      onPress={() => handleShowQRCode(index)}
+                      disabled={selectedStatus === "Upcoming"}
+                      style={
+                        selectedStatus === "In Progress"
+                          ? { backgroundColor: "green" }
+                          : null
+                      }
+                    />
+                    {showQRCodeIndex === index && (
+                      <Modal
+                        animationType="fade"
+                        transparent={true}
+                        onRequestClose={handleHideQRCode}
                       >
                         <View
                           style={[
                             {
-                              backgroundColor: Colors.primaryColor2,
-                              width: Viewport.width * 0.9,
-                              height: Viewport.height * 0.4,
-                              gap: 20,
-                              borderRadius: 10,
+                              flex: 1,
+                              backgroundColor: "rgba(0, 0, 0, 0.5)",
                             },
                             Styles.flexColumn,
                           ]}
                         >
-                          <Text
+                          <View
                             style={[
                               {
-                                fontSize: FontSizes.normal,
-                                color: Colors.primaryColor1,
-                                fontWeight: "bold",
+                                backgroundColor: Colors.primaryColor2,
+                                width: Viewport.width * 0.9,
+                                height: Viewport.height * 0.4,
+                                gap: 20,
+                                borderRadius: 10,
                               },
+                              Styles.flexColumn,
                             ]}
                           >
-                            Trip Ticket
-                          </Text>
-                          <QRCode value={JSON.stringify(schedule)} size={200} />
+                            <Text
+                              style={[
+                                {
+                                  fontSize: FontSizes.normal,
+                                  color: Colors.primaryColor1,
+                                  fontWeight: "bold",
+                                },
+                              ]}
+                            >
+                              Trip Ticket
+                            </Text>
+                            <QRCode
+                              value={JSON.stringify(schedule)}
+                              size={200}
+                            />
+                          </View>
                         </View>
-                      </View>
-                    </Modal>
-                  )}
+                      </Modal>
+                    )}
+                  </View>
                 </View>
-              </View>
-            ))
-          )}
-        </ScrollView>
+              ))
+            )}
+          </ScrollView>
+        </View>
       </View>
       <Modal
         animationType="fade"
--- src/screens/Driver/trip.tsx
+++ src/screens/Driver/trip.tsx
@@ -1,5 +1,7 @@
+import { useState, useEffect } from "react";
 import { View, Text, ScrollView, Modal } from "react-native";
 import Header from "../../components/header/header";
+import Button from "../../components/buttons/button";
 import {
   Viewport,
   FontSizes,
@@ -7,14 +9,101 @@ import {
   Colors,
 } from "../../styles/globalstyles/globalstyles";
 import { BackgroundColor } from "../../styles/globalstyles/globalstyles";
+import { todayMockData } from "../../components/mockdata/mockdata";
+import { Schedule } from "../../interfaces/interfaces";
 
 export default function Trips() {
+  const [tripData, setTripData] = useState<Schedule[]>([]);
+  const [selectedStatus, setSelectedStatus] = useState<string>("Reschedule");
+  const handleButtonPress = (status: string) => {
+    setSelectedStatus(status);
+    switch (status) {
+      case "Reschedule":
+        setTripData(todayMockData);
+        break;
+      case "Completed":
+        setTripData(todayMockData);
+        break;
+      case "Canceled":
+        setTripData(todayMockData);
+        break;
+      default:
+        setTripData([]);
+        break;
+    }
+    setSelectedStatus(status);
+  };
+  useEffect(() => {
+    handleButtonPress("Reschedule");
+  }, []);
   return (
     <>
       <BackgroundColor
         style={{ width: Viewport.width * 1, height: Viewport.height * 0.04 }}
       />
       <Header />
+      <View style={Styles.container}>
+        <View
+          style={[
+            {
+              width: Viewport.width * 0.7,
+            },
+            Styles.flexRow,
+          ]}
+        >
+          <Text
+            style={[
+              {
+                fontSize: FontSizes.normal,
+                color: Colors.primaryColor1,
+                fontWeight: "bold",
+                marginTop: 30,
+              },
+            ]}
+          >
+            Trips
+          </Text>
+        </View>
+        <View
+          style={[
+            {
+              width: Viewport.width * 1,
+              justifyContent: "space-around",
+              marginTop: 10,
+            },
+            Styles.flexRow,
+          ]}
+        >
+          <Button
+            text="Reschedule"
+            transparentBG2
+            transparentText2
+            isHighlighted={selectedStatus === "Reschedule"}
+            onPress={() => handleButtonPress("Reschedule")}
+          />
+          <Button
+            text="Completed"
+            transparentBG2
+            transparentText2
+            isHighlighted={selectedStatus === "Completed"}
+            onPress={() => handleButtonPress("Completed")}
+          />
+          <Button
+            text="Canceled"
+            transparentBG2
+            transparentText2
+            isHighlighted={selectedStatus === "Canceled"}
+            onPress={() => handleButtonPress("Canceled")}
+          />
+        </View>
+        <BackgroundColor
+          style={{
+            width: Viewport.width * 1,
+            height: Viewport.height * 0.01,
+            marginTop: Viewport.width * 0.02,
+          }}
+        />
+      </View>
     </>
   );
 }
